995a832627857a4048068625d0e2cd89c10b816e
yibeidongjie <prchen92@gmail.com>
2017-09-22 22:54:47 +0800

    maze_v2
    手动走出迷宫。通过  w s a d  控制方向。

---
+++ tree/maze/maze_v2/public.h
@@ -0,0 +1,24 @@
+#pragma once
+const int WALL = 1;
+const int ROAD = 0;
+
+// 地图的方向
+const int EAST = 0;
+const int WEST = 1;
+const int SOUTH = 2;
+const int NORTH = 3;
+
+// 人的速度
+const int FAST = 200;
+const int NORMAL = 500;
+const int SLOW = 1000;
+
+// 地图出口 人的最大移动步数
+const int OUT_X = 39;
+const int OUT_Y = 7;
+const int STEP_MAX = 2000;
+
+// 地图最大的行列
+const int MAP_ROW_MAX = 20;
+const int MAP_COL_MAX = 100;
+
